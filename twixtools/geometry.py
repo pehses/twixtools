56b730c58b06304ced56383a9ad74faa5c601239
Philip Schaten <philip.schaten@tugraz.at>
2022-07-29 17:17:59 +0200

    reformatting

--- twixtools/geometry.py
+++ twixtools/geometry.py
@@ -18,145 +18,152 @@ import twixtools
 
 
 internal_os = 2
-pcs_directions = ['dSag', 'dCor', 'dTra']
+pcs_directions = ["dSag", "dCor", "dTra"]
 
 # p. 418 - pcs to dcs
 pcs_transformations = {
-    'HFS':
-        [[  1,  0,  0 ],
-         [  0, -1,  0 ],
-         [  0,  0, -1 ]],
-    'HFP':
-        [[ -1,  0,  0 ],
-         [  0,  1,  0 ],
-         [  0,  0, -1 ]],
-    'FFS':
-       [[  -1,  0,  0 ],
-        [   0, -1,  0 ],
-        [   0,  0,  1 ]],
+    "HFS": [[1, 0, 0], [0, -1, 0], [0, 0, -1]],
+    "HFP": [[-1, 0, 0], [0, 1, 0], [0, 0, -1]],
+    "FFS": [[-1, 0, 0], [0, -1, 0], [0, 0, 1]],
 }
 
+
 def rps_to_prs():
-    return np.array(\
-        [[  0,  1,  0 ],
-         [  1,  0,  0 ],
-         [  0,  0, -1 ]])
+    return np.array([[0, 1, 0], [1, 0, 0], [0, 0, -1]])
+
 
 def get_plane_orientation(geometry):
-    if not abs(1 - np.linalg.norm(geometry['normal'])) < 0.001:
+    if not abs(1 - np.linalg.norm(geometry["normal"])) < 0.001:
         raise RuntimeError(f"Normal vector is not normal: |x| = {norm}")
 
-    maindir = np.argmax(np.abs(geometry['normal']))
+    maindir = np.argmax(np.abs(geometry["normal"]))
     if 0 == maindir:
-        mat = [[0,0,1],[0,1,0],[-1,0,0]] # @ mat // inplane mat
+        mat = [[0, 0, 1], [0, 1, 0], [-1, 0, 0]]  # @ mat // inplane mat
     if 1 == maindir:
-        mat = [[0,1,0],[0,0,1],[1,0,0]]
+        mat = [[0, 1, 0], [0, 0, 1], [1, 0, 0]]
     if 2 == maindir:
         mat = np.eye(3)
 
     init_normal = np.zeros(3)
     init_normal[maindir] = 1
 
-    v = np.cross(init_normal, geometry['normal'])
+    v = np.cross(init_normal, geometry["normal"])
     s = np.linalg.norm(v)
-    c = np.dot(init_normal, geometry['normal'])
+    c = np.dot(init_normal, geometry["normal"])
 
     if s <= 0.00001:
         mat = np.eye(3) * c @ mat
     else:
-        V = np.array(\
-            [[     0, -v[2],  v[1] ],
-             [  v[2],     0, -v[0] ],
-             [ -v[1],  v[0],     0 ]])
-        mat = (np.eye(3)  + V + V*V*(1-c)/s**2) @ mat
+        V = np.array([[0, -v[2], v[1]], [v[2], 0, -v[0]], [-v[1], v[0], 0]])
+        mat = (np.eye(3) + V + V * V * (1 - c) / s ** 2) @ mat
 
     return mat
 
+
 def get_inplane_rotation(geometry):
     mat = [
-        [ -np.sin(geometry['angle']),  np.cos(geometry['angle']), 0],
-        [ -np.cos(geometry['angle']), -np.sin(geometry['angle']), 0],
-        [  0,                          0,                         1],
-      ]
+        [-np.sin(geometry["angle"]), np.cos(geometry["angle"]), 0],
+        [-np.cos(geometry["angle"]), -np.sin(geometry["angle"]), 0],
+        [0, 0, 1],
+    ]
     return np.array(mat)
+
 
 def prs_to_pcs(geometry):
     mat = get_inplane_rotation(geometry)
     mat = get_plane_orientation(geometry) @ mat
     return mat
 
+
 def pcs_to_xyz(g):
-    if g['patient_position'] in pcs_transformations:
-        return np.array(pcs_transformations[g['patient_position']])
+    if g["patient_position"] in pcs_transformations:
+        return np.array(pcs_transformations[g["patient_position"]])
     else:
         raise RuntimeError(f"Unknown patient position: {g['patient_position']}")
+
 
 def prs_to_xyz(g):
     return pcs_to_xyz(g) @ prs_to_pcs(g)
 
+
 def rps_to_xyz(g):
     return prs_to_xyz(g) @ rps_to_prs()
+
 
 def get_geometry(twix):
     geometry = {}
 
-    if twix['hdr']['MeasYaps']['sKSpace']['ucDimension'] == 2:
-        geometry['dims'] = 2
-    elif twix['hdr']['MeasYaps']['sKSpace']['ucDimension'] == 4:
-        geometry['dims'] = 3
+    if twix["hdr"]["MeasYaps"]["sKSpace"]["ucDimension"] == 2:
+        geometry["dims"] = 2
+    elif twix["hdr"]["MeasYaps"]["sKSpace"]["ucDimension"] == 4:
+        geometry["dims"] = 3
     else:
-        geometry['dims'] = None
+        geometry["dims"] = None
 
-    if len(twix['hdr']['MeasYaps']['sSliceArray']['asSlice']) > 1:
+    if len(twix["hdr"]["MeasYaps"]["sSliceArray"]["asSlice"]) > 1:
         print("WARNING more than one slice. Taking first one..")
 
-    geometry['fov'] = [
-        twix['hdr']['MeasYaps']['sSliceArray']['asSlice'][0]['dReadoutFOV'] * internal_os,
-        twix['hdr']['MeasYaps']['sSliceArray']['asSlice'][0]['dPhaseFOV'],
-        twix['hdr']['MeasYaps']['sSliceArray']['asSlice'][0]['dThickness']
+    geometry["fov"] = [
+        twix["hdr"]["MeasYaps"]["sSliceArray"]["asSlice"][0]["dReadoutFOV"]
+        * internal_os,
+        twix["hdr"]["MeasYaps"]["sSliceArray"]["asSlice"][0]["dPhaseFOV"],
+        twix["hdr"]["MeasYaps"]["sSliceArray"]["asSlice"][0]["dThickness"],
     ]
 
-    geometry['resolution'] = [
-        twix['hdr']['MeasYaps']['sKSpace']['lBaseResolution'] * internal_os,
-        twix['hdr']['MeasYaps']['sKSpace']['lPhaseEncodingLines'],
-        twix['hdr']['MeasYaps']['sKSpace']['lPartitions'] if geometry['dims'] == 3 else 1
+    geometry["resolution"] = [
+        twix["hdr"]["MeasYaps"]["sKSpace"]["lBaseResolution"] * internal_os,
+        twix["hdr"]["MeasYaps"]["sKSpace"]["lPhaseEncodingLines"],
+        twix["hdr"]["MeasYaps"]["sKSpace"]["lPartitions"]
+        if geometry["dims"] == 3
+        else 1,
     ]
 
-    geometry['voxelsize'] = list(np.array(geometry['fov']) / np.array(geometry['resolution']))
+    geometry["voxelsize"] = list(
+        np.array(geometry["fov"]) / np.array(geometry["resolution"])
+    )
 
-    geometry['normal'] = [0,0,0]
-    if 'sNormal' in twix['hdr']['MeasYaps']['sSliceArray']['asSlice'][0]:
-        for i,d in enumerate(pcs_directions):
-            geometry['normal'][i] = twix['hdr']['MeasYaps']['sSliceArray']['asSlice'][0]['sNormal'].get(d, geometry['normal'][i])
+    geometry["normal"] = [0, 0, 0]
+    if "sNormal" in twix["hdr"]["MeasYaps"]["sSliceArray"]["asSlice"][0]:
+        for i, d in enumerate(pcs_directions):
+            geometry["normal"][i] = twix["hdr"]["MeasYaps"]["sSliceArray"]["asSlice"][
+                0
+            ]["sNormal"].get(d, geometry["normal"][i])
 
-    geometry['offset'] = [0,0,0]
-    if 'sPosition' in twix['hdr']['MeasYaps']['sSliceArray']['asSlice'][0]:
-        for i,d in enumerate(pcs_directions):
-            geometry['offset'][i] = twix['hdr']['MeasYaps']['sSliceArray']['asSlice'][0]['sPosition'].get(d, geometry['offset'][i])
+    geometry["offset"] = [0, 0, 0]
+    if "sPosition" in twix["hdr"]["MeasYaps"]["sSliceArray"]["asSlice"][0]:
+        for i, d in enumerate(pcs_directions):
+            geometry["offset"][i] = twix["hdr"]["MeasYaps"]["sSliceArray"]["asSlice"][
+                0
+            ]["sPosition"].get(d, geometry["offset"][i])
 
-    geometry['angle'] = twix['hdr']['MeasYaps']['sSliceArray']['asSlice'][0].get('dInPlaneRot',0)
+    geometry["angle"] = twix["hdr"]["MeasYaps"]["sSliceArray"]["asSlice"][0].get(
+        "dInPlaneRot", 0
+    )
 
-    if 'tPatientPosition' in twix['hdr']['Meas']:
-        geometry['patient_position'] = twix['hdr']['Meas'].get('tPatientPosition')
-    elif 'sPatPosition' in twix['hdr']['Meas']:
-        geometry['patient_position'] = twix['hdr']['Meas'].get('sPatPosition')
+    if "tPatientPosition" in twix["hdr"]["Meas"]:
+        geometry["patient_position"] = twix["hdr"]["Meas"].get("tPatientPosition")
+    elif "sPatPosition" in twix["hdr"]["Meas"]:
+        geometry["patient_position"] = twix["hdr"]["Meas"].get("sPatPosition")
     else:
-        geometry['patient_position'] = None
+        geometry["patient_position"] = None
 
-    geometry['matrix'] = rps_to_xyz(geometry).tolist()
+    geometry["matrix"] = rps_to_xyz(geometry).tolist()
 
     return geometry
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("raw", help="Raw data file")
-    parser.add_argument("outfile", nargs="?", default="-", help=".json file to store the output")
+    parser.add_argument(
+        "outfile", nargs="?", default="-", help=".json file to store the output"
+    )
     args = parser.parse_args()
 
     twix = twixtools.read_twix(args.raw, parse_data=False)
     geometry = get_geometry(twix[-1])
-    if args.outfile == '-':
+    if args.outfile == "-":
         print(json.dumps(geometry, indent=4, sort_keys=True))
     else:
-        with open(args.outfile, 'w') as f:
+        with open(args.outfile, "w") as f:
             json.dump(geometry, f)
--- utils/rotate_3Dcfl.py
+++ utils/rotate_3Dcfl.py
@@ -1,75 +1,93 @@
 #!/usr/bin/python
 import numpy as np
 
+
 def apply_transform(g, data):
     import scipy.ndimage
+
     data = resample(g, data)
-    a = _augment_matrix_dims(np.array(g['matrix']), len(data.shape))
+    a = _augment_matrix_dims(np.array(g["matrix"]), len(data.shape))
     A = np.array(np.linalg.inv(a))
-    output_shape,offset = get_newshape(data.shape, a)
-    return scipy.ndimage.affine_transform(data, A, offset=offset, output_shape=output_shape)
+    output_shape, offset = get_newshape(data.shape, a)
+    return scipy.ndimage.affine_transform(
+        data, A, offset=offset, output_shape=output_shape
+    )
+
 
 def get_newshape(shape, mat):
-    edges=get_edges(shape)
+    edges = get_edges(shape)
 
-    for i,edge in enumerate(edges):
+    for i, edge in enumerate(edges):
         edges[i] = mat @ edge
-    #get bottom left edge.
+    # get bottom left edge.
     bottom = np.amin(edges, axis=0)
     shifted_edges = np.array([edge - bottom for edge in edges])
     # new top right edge
     output_shape = np.amax(shifted_edges, axis=0)
-    offset = np.array(shape)/2 - np.linalg.inv(mat) @ (output_shape/2)
+    offset = np.array(shape) / 2 - np.linalg.inv(mat) @ (output_shape / 2)
 
     output_shape = tuple([int(x) for x in output_shape])
     print(output_shape, offset)
 
     return output_shape, offset
 
+
 def get_edges(shape):
     edges = []
     # iterate over all possible selections of dimensions
-    for bitmask in range(2**len(shape)):
+    for bitmask in range(2 ** len(shape)):
         edge = np.zeros(len(shape))
         for dim in range(len(shape)):
-            if bitmask & 1<<dim:
-                edge[dim]=shape[dim]
+            if bitmask & 1 << dim:
+                edge[dim] = shape[dim]
         edges.append(edge)
     return edges
 
+
 def resample(g, data):
     import scipy.interpolate
+
     def get_isotropic_dims(g):
-        ref_vs = np.mean(g['voxelsize'])
-        dims = np.copy(g['resolution'])
-        for i,vs in enumerate(g['voxelsize']):
+        ref_vs = np.mean(g["voxelsize"])
+        dims = np.copy(g["resolution"])
+        for i, vs in enumerate(g["voxelsize"]):
             if abs(vs - ref_vs) > 5 / dims[i] * vs:
                 dims[i] = dims[i] * vs / ref_vs
         return dims
 
     newdims = get_isotropic_dims(g)
-    maxdist = max(np.abs(g['resolution']) - np.array(newdims))
+    maxdist = max(np.abs(g["resolution"]) - np.array(newdims))
     if maxdist > 5:
-        print(f"""
+        print(
+            f"""
         Old dims: {g['resolution']}
         Old voxelsize: {g['voxelsize']};
         New dims: {newdims};
         New voxelsize: {g['fov']/newdims}
-        """)
-        g['iso_dims'] = newdims.tolist()
-        g['iso_voxelsize'] = (g['fov']/newdims).tolist()
+        """
+        )
+        g["iso_dims"] = newdims.tolist()
+        g["iso_voxelsize"] = (g["fov"] / newdims).tolist()
 
         dims = data.shape
-        coor = [ (np.arange(s) - (s-1)/2)/s for s in dims]
-        coor_neu = [ (np.arange(s) - (s-1)/2)/s for s in newdims]
-        target_grid = np.stack(np.meshgrid(*coor_neu, indexing='ij'), axis=-1)
-        return scipy.interpolate.interpn(tuple(coor), data, target_grid, bounds_error=False, fill_value=None, method='linear')
+        coor = [(np.arange(s) - (s - 1) / 2) / s for s in dims]
+        coor_neu = [(np.arange(s) - (s - 1) / 2) / s for s in newdims]
+        target_grid = np.stack(np.meshgrid(*coor_neu, indexing="ij"), axis=-1)
+        return scipy.interpolate.interpn(
+            tuple(coor),
+            data,
+            target_grid,
+            bounds_error=False,
+            fill_value=None,
+            method="linear",
+        )
     else:
         return data
 
+
 def _augment_matrix_dims(a, N):
     e = np.eye(N)
-    e[:a.shape[0],:a.shape[1]] = a[:,:]
+    e[: a.shape[0], : a.shape[1]] = a[:, :]
     return e
 
 
@@ -80,17 +98,20 @@ def create_transform_file(transform, raw):
     twix = twixtools.read_twix(raw, parse_data=False)
     g = geometry.get_geometry(twix[-1])
 
-    with open(transform, 'w') as f:
+    with open(transform, "w") as f:
         json.dump(g, f, indent=4)
+
 
 def apply_transform_file(transform, infile, outfile):
     from twixtools.contrib import cfl
+
     data = cfl.readcfl(infile)
-    with open(transform,'r') as f:
+    with open(transform, "r") as f:
         g = json.load(f)
 
     data = apply_transform(g, data)
     cfl.writecfl(outfile, data)
+
 
 if __name__ == "__main__":
     import argparse
@@ -98,8 +119,13 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("transform", help="JSON File for transformation")
-    parser.add_argument("infile", help="Either .dat file or cfl input file to be transformed")
-    parser.add_argument("outfile", nargs="?", default=None, help="Transformed output (optional)")
+    parser.add_argument(
+        "infile",
+        help="Either .dat file - then JSON File for transformation will be created -  or cfl input file to be transformed",
+    )
+    parser.add_argument(
+        "outfile", nargs="?", default=None, help="Transformed output (optional)"
+    )
     args = parser.parse_args()
 
     if args.outfile:
